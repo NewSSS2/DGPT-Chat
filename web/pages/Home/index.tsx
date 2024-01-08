7eaf02db899e6534c3242e73a8e6268661736b54
SSS2 <141389229+NewSSS2@users.noreply.github.com>
2024-01-07 22:45:15 -0500

    Final Bunny 2

--- web/pages/Home/index.tsx
+++ web/pages/Home/index.tsx
@@ -57,7 +57,7 @@ const HomePage: Component = () => {
           aria-labelledby="homeTitle"
         >
           <span id="homeTitle" aria-hidden="true">
-            Final<span class="text-[var(--hl-500)]">Bunny</span>
+            ReallyFinal<span class="text-[var(--hl-500)]">Bunny</span>
           </span>
         </div>
 
